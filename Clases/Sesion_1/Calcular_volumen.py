290c83b3b2a0354d0fcc901b9e806f9551d14735
303 - Programacion <303@laboratorios.usbbog.edu.co>
2023-08-17 13:57:18 -0500

    Actualizacion1

---
+++ Clases/Sesion_1/Calcular_volumen.py
@@ -0,0 +1,4 @@
+r = int (input("Ingrese una cantidad de radio: "))
+v = ((4/3)*3.1416*r**3)
+vl= v* 1000
+print(f"El volumen de un recipiente de radio  {round (r)} metros es  {round (vl,2)} litros")
---
+++ Clases/Sesion_1/Cuadratica.py
@@ -0,0 +1,10 @@
+import math as m
+
+a = int(input(" Ingrese a: "))
+b = int(input(" Ingrese b: "))
+c = int(input(" Ingrese c: "))
+
+x1 = (-b+m.sqrt(b**2 -4*a*c))/(2*a)
+x2 = (-b-m.sqrt(b**2 -4*a*c))/(2*a)
+
+print(f"Las soluciones son {x1} y {x2}")
